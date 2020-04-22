d0d41b0e09684f2897bf2e866a0f86ac36feeb26
javier galvez <javier.galvezg@utem.cl>
2020-04-21 21:43:52 -0400

    eliminacion de variables no utilizadas e ingreso de comentarios

--- tarea1.cpp
+++ tarea1.cpp
@@ -6,27 +6,30 @@
 using namespace std;
 
 int contar_letras(string x){
-    int y, i, cont, k,j=0;
+    int y, i, cont;// definir variables
     char b;
-    string c;
-    y=x.length();
-    k=0;
+    y=x.length();//obtenemos el largo de la palabra
     cont=0;
-    for(i=0; i< y ; i++){
+    /*recorremos la palabra desde la posicion 0,y luego se comprueba si son iguales las 2 letras adyacentes.
+    En el caso de que si, se aumenta el contador, si no se realiza la union de la letra y la cantidad de
+    veces que se repite*/
+    for(i=0; i< y ; i++){ 
         b=x[i];
         if(x[i+1]==b){
             cont++;
         }
         else{
+            /*la union de la letra y la cantidad de veces que se repetia fue lo que mas que costo, por lo que 
+            tuve que revisar en internet como lograrlo, gracias a eso logre que funcionara*/
           ostringstream prueba;
           prueba << b << cont+1;
           cout<< prueba.str();
-          cont=0;
+          cont=0;//se reinicia el contador para la siguiente letra
         }
     }
 }
 
-main(){
+int main(){//se le pide al usuario ingresar ua palabre para luego llamar a la funcion 
     string palabra;
     cout<< "ingrese una palabra \n";
     cin >> palabra;
